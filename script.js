eaa86358d9dac39524dbde07b80d1bde49753934
JoaquinG33 <joaquinguillen33@hotmail.com>
2025-07-28 16:16:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 
 //DARK/LIGHT MODE
-const modeSwitch = document.getElementById("modeSwitch");
+// const modeSwitch = document.getElementById("modeSwitch");
 
-let lightMode = localStorage.getItem("lightMode") === "enabled";
+// let lightMode = localStorage.getItem("lightMode") === "enabled";
 
-const enableLightmode = () => {
-    document.body.classList.add("lightMode");
-    localStorage.setItem("lightMode", "enabled");
+// const enableLightmode = () => {
+//     document.body.classList.add("lightMode");
+//     localStorage.setItem("lightMode", "enabled");
+// }
+
+// const disableLightmode = () => {
+//     document.body.classList.remove("lightMode");
+//     localStorage.setItem("lightMode", "disabled");
+// }
+
+
+// if (lightMode) enableLightmode();
+
+// modeSwitch.addEventListener("click", () => {
+//     lightMode = !lightMode;
+//     lightMode ? enableLightmode() : disableLightmode();
+// });
+
+//Actualizar cumpleaños
+
+let edad = document.querySelector("#edad");
+let edadNumero = 28;
+
+const fecha = new Date();
+let diaCumple = fecha.getDate();
+let mesCumple = fecha.getMonth() + 1;
+
+
+function actualizarEdad(){
+    if((diaCumple = 23) & (mesCumple = 7)){
+ edadNumero++;
+ edad.innerText = edadNumero;
+
+    }
 }
-
-const disableLightmode = () => {
-    document.body.classList.remove("lightMode");
-    localStorage.setItem("lightMode", "disabled");
-}
+actualizarEdad();
 
 
-if (lightMode) enableLightmode();
-
-modeSwitch.addEventListener("click", () => {
-    lightMode = !lightMode;
-    lightMode ? enableLightmode() : disableLightmode();
-});
