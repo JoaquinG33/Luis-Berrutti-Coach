09c7a201d1fc48c4951ad8144dfb4cfd0b661434
JoaquinG33 <joaquinguillen33@hotmail.com>
2025-07-30 19:52:38 -0300

    Version 1.0

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
+//Joaquín Guillén. 2025.
 
-//DARK/LIGHT MODE
-// const modeSwitch = document.getElementById("modeSwitch");
-
-// let lightMode = localStorage.getItem("lightMode") === "enabled";
-
-// const enableLightmode = () => {
-//     document.body.classList.add("lightMode");
-//     localStorage.setItem("lightMode", "enabled");
-// }
-
-// const disableLightmode = () => {
-//     document.body.classList.remove("lightMode");
-//     localStorage.setItem("lightMode", "disabled");
-// }
-
-
-// if (lightMode) enableLightmode();
-
-// modeSwitch.addEventListener("click", () => {
-//     lightMode = !lightMode;
-//     lightMode ? enableLightmode() : disableLightmode();
-// });
 
 //Actualizar cumpleaños
 
